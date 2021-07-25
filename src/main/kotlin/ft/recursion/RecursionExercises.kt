d915fd93c3e5d010fe2fc4ea02085d16d07791be
Andrew Parker <andrew@avparker.com>
2021-07-25 22:04:17 +0900

    Implement part two - Recursion

--- src/main/kotlin/ft/recursion/RecursionExercises.kt
+++ src/main/kotlin/ft/recursion/RecursionExercises.kt
@@ -41,36 +41,93 @@ object RecursionExercises {
   fun minusOne(n: Int) = n - 1
 
   // Add two non-negative Integers together.  You are only allowed to use plusOne and minusOne above
-  fun add(a: Int, b: Int): Int = TODO()
+  tailrec fun add(a: Int, b: Int): Int =
+    if (b == 0)
+      a
+    else
+      add(plusOne(a), minusOne(b))
 
   // You are not permitted to use any list functions such as map, flatMap, ++, flatten etc
-   fun sum(l: FunList<Int>): Int = TODO()
+   fun sum(l: FunList<Int>): Int = when(l) {
+     is Cons -> {
+       val (head, tail) = l
+       head + sum(tail)
+     }
+     else -> 0
+   }
 
   //Again no list functions are permitted for the following
-  fun <A> length(x: FunList<A>): Int = TODO()
+  fun <A> length(x: FunList<A>): Int = when(x) {
+    is Cons -> {
+      val (head, tail) = x
+      1 + length(tail)
+    }
+    else -> 0
+  }
 
   // Do you notice anything similar between sum and length? Hmm...
 
   // Mapping over a list.  You are given a FunList of type A and a function converting an A to a B
   // and you give back a list of type B.  No list functions allowed!
-  fun <A, B> map(x: FunList<A>, f: (A) -> B): FunList<B> = TODO()
+  fun <A, B> map(x: FunList<A>, f: (A) -> B): FunList<B> = when(x) {
+    is Cons -> {
+      val (head, tail) = x
+//      f(head) :: map(tail, f) // is this only valid in scala?
+      Cons(f(head), map(tail, f))
+    }
+    else -> Nil
+  }
 
   // Given a function from A -> Boolean, return a list with only those item where the function returned true.
-  fun <A> filter(x: FunList<A>, f: (A) -> Boolean): FunList<A> = TODO()
+  fun <A> filter(x: FunList<A>, f: (A) -> Boolean): FunList<A> = when(x) {
+    is Cons -> {
+      val (head, tail) = x
+      if (f(head))
+        Cons(head, filter(tail, f))
+      else
+        filter(tail, f)
+    }
+    else -> Nil
+  }
 
   // This pattern should be familiar by now... psst... look at add.
-  fun <A> append(x: FunList<A>, y: FunList<A>): FunList<A> = TODO()
+  fun <A> append(x: FunList<A>, y: FunList<A>): FunList<A> = when(x) {
+    is Cons -> {
+      val (head, tail) = x
+      Cons(head, append(tail, y))
+    }
+    else -> y
+  }
 
   // Flatten a list of lists to a single list.  Remember you can't use list.flatten.  Can you use a previous
   // solution to solve this one?
-  fun <A> flatten(x: FunList<FunList<A>>): FunList<A> = TODO()
+  fun <A> flatten(x: FunList<FunList<A>>): FunList<A> = when(x) {
+    is Cons -> {
+      val (head, tail) = x
+      append(head, flatten(tail))
+    } else -> Nil
+  }
 
   // Follow the types.  You've done a great job getting here. Follow the types.
-  fun <A, B> flatMap(x: FunList<A>, f: (A) -> FunList<B>): FunList<B> = TODO()
+  fun <A, B> flatMap(x: FunList<A>, f: (A) -> FunList<B>): FunList<B> =
+    flatten(map(x, f))
 
   // Maximum of the empty list is 0
-  fun maximum(x: FunList<Int>): Int = TODO()
+  fun maximum(x: FunList<Int>): Int = when(x) {
+    is Cons -> {
+      val (head, tail) = x
+      val maxTail = maximum(tail)
+      if (head >= maxTail) head else maxTail
+    }
+    else -> 0
+  }
 
   // Reverse a list
-  fun <A> reverse(x: FunList<A>): FunList<A> = TODO()
+  fun <A> reverse(x: FunList<A>): FunList<A> = when(x) {
+    is Cons -> {
+      val (head, tail) = x
+      append(reverse(tail), Cons(head, Nil))
+    }
+    else -> Nil
+  }
 }
